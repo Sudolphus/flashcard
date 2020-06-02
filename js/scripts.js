33f4c38a5c1811bd958ed12e54acc929e15201d3
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-02 14:11:41 -0700

    add rest of javascript to wells
    co-authored-by: Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,93 @@ $(document).ready(function() {
     $("#javaDef").toggle();
   })
 
-  $(".clickOperator").click(function(){
+  $(".clickOperator").click(function() {
     $("#operatorName").toggle();
     $("#operatorDef").toggle();
   })
 
+  $(".clickVariable").click(function() {
+    $("#variableName").toggle();
+    $("#variableDef").toggle();
+  })
+  
+  $(".clickVariableNamingConventions").click(function() {
+    $("#variableNamingConventionsName").toggle();
+    $("#variableNamingConventionsDef").toggle();
+  })
+
+  $(".clickFunctions").click(function() {
+    $("#functionsName").toggle();
+    $("#functionsDef").toggle();
+  })
+
+  $(".clickMethods").click(function() {
+    $("#methodsName").toggle();
+    $("#methodsDef").toggle();
+  })
+
+  $(".clickArguments").click(function() {
+    $("#argumentsName").toggle();
+    $("#argumentsDef").toggle();
+  })
+
+  $(".clickParameters").click(function() {
+    $("#parametersName").toggle();
+    $("#parametersDef").toggle();
+  })
+
+  $(".clickReturn").click(function() {
+    $("#returnName").toggle();
+    $("#returnDef").toggle();
+  })
+
+  $(".clickChainingMethods").click(function() {
+    $("#chainingMethodsName").toggle();
+    $("#chainingMethodsDef").toggle();
+  })
+
+  $(".clickStrings").click(function() {
+    $("#stringsName").toggle();
+    $("#stringsDef").toggle();
+  })
+
+  $(".clickBooleans").click(function() {
+    $("#booleansName").toggle();
+    $("#booleansDef").toggle();
+  })
+
+  $(".clickUndefined").click(function() {
+    $("#undefinedName").toggle();
+    $("#undefinedDef").toggle();
+  })
+
+  $(".clickNaN").click(function() {
+    $("#naNName").toggle();
+    $("#naNDef").toggle();
+  })
+
+  $(".clickNull").click(function() {
+    $("#nullName").toggle();
+    $("#nullDef").toggle();
+  })
+
+  $(".clickAlert").click(function() {
+    $("#alertName").toggle();
+    $("#alertDef").toggle();
+  })
+
+  $(".clickComments").click(function() {
+    $("#commentsName").toggle();
+    $("#commentsDef").toggle();
+  })
+
+  $(".clickjQuery").click(function() {
+    $("#jQueryName").toggle();
+    $("#jQueryDef").toggle();
+  })
+
+  $(".clickAttributes").click(function() {
+    $("#attributesName").toggle();
+    $("#attributesDef").toggle();
+  })
 })
